a2f1b392b5a7b1ff61dbecb2a8c01465029e425c
Abhinav Sharma <abhinavsharma392@gmail.com>
2025-10-31 18:41:35 +0530

    fixed db error

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -91,7 +91,7 @@ function ZapTable({ zaps }: {zaps: Zap[]}) {
         {zaps.map(z => <div className="flex border-b border-t py-4">
             <div className="flex-1 flex"><img src={z.trigger.type.image} className="w-[30px] h-[30px]" /> {z.actions.map(x => <img src={x.type.image} className="w-[30px] h-[30px]" />)}</div>
             <div className="flex-1">{z.id}</div>
-            <div className="flex-1">Nov 13, 2023</div>
+            <div className="flex-1">Nov 1, 2025</div>
             <div className="flex-1">{`${HOOKS_URL}/hooks/catch/1/${z.id}`}</div>
             <div className="flex-1"><LinkButton onClick={() => {
                     router.push("/zap/" + z.id)
